6a523996c91fe3026315f507d2d1571208140339
shb03323 <39729721+shb03323@users.noreply.github.com>
2019-04-16 17:39:35 +0900

    Update Friend.java

--- Friend.java
+++ Friend.java
@@ -1,60 +1,50 @@
-
 public class Friend {
-    private String name; //이름
-    private int group; //친구 그룹
-    private String email; //이메일
-    private String profilePic;
-    private String phoneNumber;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getGroup() {
-        return group;
-    }
-
-    public void setGroup(int group) {
-        this.group = group;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getProfilePic() {
-        return profilePic;
-    }
-
-    public void setProfilePic(String profilePic) {
-        this.profilePic = profilePic;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Friend(String name, int group, String phoneNumber, String email, String profilePic) {
-        this.name = name;
-        this.group = group;
-        this.email = email;
-        this.profilePic = profilePic;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void print() {
-        System.out.println(name + " : " + group + " : " + phoneNumber + " : " + email + " : " + profilePic);
-    }
+   private String name;
+   private int group;
+   private String num;
+   private String email;
+   private String img;
+   
+   public String getName() {
+      return name;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+   public int getGroup() {
+      return group;
+   }
+   public void setGroup(int group) {
+      this.group = group;
+   }
+   public String getNum() {
+      return num;
+   }
+   public void setNum(String num) {
+      this.num = num;
+   }
+   public String getEmail() {
+      return email;
+   }
+   public void setEmail(String email) {
+      this.email = email;
+   }
+   public String getImg() {
+      return img;
+   }
+   public void setImg(String img) {
+      this.img = img;
+   }
+   
+   public Friend(String name, int group, String num, String email, String img) {
+      this.name = name;
+      this.group = group;
+      this.num = num;
+      this.email = email;
+      this.img =img;
+   }
+   
+   public void print() {
+      System.out.println("Name : " + name + "Group : " + group +  "Phone Number : " + num + "Email : " + email +"Image : " + img);
+   }
 }
