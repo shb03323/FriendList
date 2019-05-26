983f4deb264d82b3b9508ce9ef33a620b306a3aa
shb03323 <39729721+shb03323@users.noreply.github.com>
2019-05-27 00:15:10 +0900

    Update Friend.java

--- src/Friend.java
+++ src/Friend.java
@@ -1,61 +1,54 @@
+public class Friend {
+   private String name;
+   private String group;
+   private String num;
+   private String email;
+   private String img;
+   
+   public String getName() {
+      return name;
+   }
+   public void setName(String name) {
+      this.name = name;
+   }
+   public String getGroup() {
+      return group;
+   }
+   public void setGroup(String group) {
+      this.group = group;
+   }
+   public String getNum() {
+      return num;
+   }
+   public void setNum(String num) {
+      this.num = num;
+   }
+   public String getEmail() {
+      return email;
+   }
+   public void setEmail(String email) {
+      this.email = email;
+   }
+   public String getImg() {
+      return img;
+   }
+   public void setImg(String img) {
+      this.img = img;
+   }
 
-
-class Friend {
-    private String name;
-    private int group;
-    private String email;
-    private String profilePic;
-    private String phoneNumber;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getGroup() {
-        return group;
-    }
-
-    public void setGroup(int group) {
-        this.group = group;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getProfilePic() {
-        return profilePic;
-    }
-
-    public void setProfilePic(String profilePic) {
-        this.profilePic = profilePic;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Friend(String name, int group, String phoneNumber, String email, String profilePic) {
-        this.name = name;
-        this.group = group;
-        this.email = email;
-        this.profilePic = profilePic;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void print() {
-        System.out.println(name + " : " + group + " : " + phoneNumber + " : " + email + " : " + profilePic);
-    }
+   public Friend(String name, String group, String num, String email, String img) {
+      this.name = name;
+      this.group = group;
+      this.num = num;
+      this.email = email;
+      this.img =img;
+   }
+   
+   public void print() {
+      System.out.println("Name : " + name);
+      System.out.println("Group : " + group);
+      System.out.println("Phone Number : " + num);
+      System.out.println("Email : " + email);
+      System.out.println("Image : " + img);
+   }
 }
